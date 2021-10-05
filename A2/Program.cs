50bfa761ab688d7162e6fff29bf6e2a4a9b2828e
Dhruvkaneriya <32631492+Dhruvkaneriya@users.noreply.github.com>
2021-10-04 21:29:30 -0400

    commit

--- A2/Program.cs
+++ A2/Program.cs
@@ -20,10 +20,11 @@ namespace A2
 			else
 			{
 				Console.WriteLine("Please enter a valid number.");
-			}
+			} 
 
-			// second method
-			Random random = new Random(1);
+            Console.WriteLine();
+
+			// second program
 			ParticleMovement particleMover = new ParticleMovement();
 			while (true)
 			{
@@ -31,11 +32,11 @@ namespace A2
 							  "2 if a gravitational field is present\n3 for both fields\n");
 				char key = Console.ReadKey().KeyChar;
 				if (key != '0' && key != '1' && key != '2' && key != '3') return;
-				int movementRange = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
-				particleMover.MovementRange = movementRange;
-				//particleMover.SetMagneticField(key == '1' || key == '3');
-				//particleMover.SetGravitationalField(key == '2' || key == '3');
-				Console.WriteLine($"\nParticle with a movement range of {movementRange} units" +
+				
+				// 10. I am call MagneticField and GravitationalField in main method.
+				particleMover.MagneticField = (key == '1' || key == '3');
+				particleMover.GravitationalField = (key == '2' || key == '3');
+				Console.WriteLine($"\nParticle with a movement range of {particleMover.MovementRange} units" +
 								  $" moved a total distance of {particleMover.DistanceMoved} units.\n");
 			}
 		}
@@ -43,40 +44,75 @@ namespace A2
 		// second method class 
 		class ParticleMovement
 		{
+			static Random random = new Random(1);
+
+			// 7. I am making Xml documentation code
+			/// <summary>
+			/// Also i will explain all public class members in this xml code.
+			/// </summary>
+			/// <param name="BASE_Movement">Here i will make constant base movement variable.</param>
+			/// <param name="GRAVITY_MOVEMENT">Here i will make constant gravity movement variable.</param>
+			/// <param name="MovementRange">Here i will make movementRange property. </param>
+			/// <param name="MagneticField">Here i will make MagneticField property.</param>
+			/// <param name="GravitationalField">Here i will make GravitationalField property.</param>
+			/// <param name="Distance">Here i will make auto-property "Distance".</param>
+			/// <returns></returns>
+
 			public const int BASE_MOVEMENT = 3;
 			public const int GRAVITY_MOVEMENT = 2;
 
+			// 1. I am making MovementRange property with getter and setter values.
 			private int movementRange;
 			public int MovementRange
 			{
 				get { return movementRange; }
-				set { movementRange = value; CalculateDistance(); }
+				set { movementRange = value;}
 			}
 
-			public int DistanceMoved;
-
-			public double Distance
-			{ get; set; }
-
-			public void CalculateDistance()
-			{
-				if (GravitationalField == true && MagneticField == true)
-				DistanceMoved = MovementRange + BASE_MOVEMENT + GRAVITY_MOVEMENT;
-			}
-
+			// 3. I am making MagneticField property with getter and setter values.
 			private bool magneticField;
 			public bool MagneticField
 			{
 				get { return magneticField; }
-				set { magneticField = value; CalculateDistance(); }
+				set { magneticField = value; }
 			}
 
+			// 2. I am making GravitationalField property with getter and setter values.
 			private bool gravitationalField;
 			public bool GravitationalField
 			{
 				get { return gravitationalField; }
-				set { gravitationalField = value; CalculateDistance(); }
+				set { gravitationalField = value; }
 			}
+
+			public int DistanceMoved;
+
+			// 4. I am making auto-implemented property Distance with public and private accessor.
+			public double Distance
+			{ get; private set; }
+
+			// 5. I am deleting MagneticMultiplier and the GravityMovement field and update CalculateDistance() method.
+			public void CalculateDistance()
+			{
+				if (GravitationalField == true && MagneticField == true)
+					DistanceMoved = MovementRange + BASE_MOVEMENT + GRAVITY_MOVEMENT;
+			}
+
+			// 6. I am making Particalemovement constructor and call CalculateDistance() method.
+			private int movement_range;
+			public ParticleMovement()
+			{
+				movementRange = movement_range;
+				CalculateDistance();
+				GetMovementRange(); // 9. I am calling GetMovementRange() method in constructor.
+			}
+
+			// 8. I am making GetMovementRange() method and calculate movementRange.
+			public void GetMovementRange()
+			{
+				movementRange = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+			}
+
 		}
 
 		// first method class
@@ -112,12 +148,13 @@ namespace A2
 				return pickedCards;
 			}
 
+			// I am making cards values like "Ace", "Jack", "Queen", "King" enumaration instad of string.
 			enum Values
 			{
 				Ace,
 				Jack,
 				Queen,
-				King,
+				King
 			}
 
 			private static string RandomValue()
@@ -125,6 +162,8 @@ namespace A2
 				int value = random.Next(1, 14);
 				switch (value)
 				{
+					// In switch funcation I am calling cards values enumaration.
+
 					case 1:
 						Values One = Values.Ace;
 						return One.ToString();
@@ -143,12 +182,13 @@ namespace A2
 				}
 			}
 
+			// I am making cards suits like "Clubs", "Diamonds", "Hearts", "Spades" enumaration instad of string.
 			enum Suits
 			{
 				Clubs,
 				Diamonds,
 				Hearts,
-				Spades,
+				Spades
 			}
 
 			private static string RandomSuit()
@@ -156,6 +196,8 @@ namespace A2
 				int value = random.Next(1, 5);
 				switch (value)
 				{
+					// In switch funcation I am calling cards suits enumaration.
+
 					case 1:
 						Suits C = Suits.Clubs;
 						return C.ToString();
